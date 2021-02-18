27b47adcf1561da0d01d8edd6bd642b818430c71
nikit34 <permikov134@mail.ru>
2021-02-18 16:41:13 +0300

    ==

--- long_arith.cpp
+++ long_arith.cpp
@@ -9,12 +9,13 @@ class BigInteger {
 public:
     BigInteger(string& str);
     BigInteger(signed long long& digits);
-    BigInteger::BigInteger(unsigned long long& digits);
+    BigInteger(unsigned long long& digits);
     void RemoveLeadZeros();
+    friend bool operator==(const BigInteger& left, const BigInteger& right);
 
 private:
 	static const uint64_t BASE = 1000000000;
-	std::vector<int> digits;
+	std::vector<uint16_t> digits;
 	bool is_negative;
 };
 
@@ -65,62 +66,82 @@ BigInteger::BigInteger(signed long long& digits) {
 }
 
 BigInteger::BigInteger(unsigned long long& digits) {
-        this->is_negative = false;
-        do {
-            this->digits.push_back(digits % BigInteger::BASE);
-                digits /= BigInteger::BASE;
-        } while (digits != 0);
+    this->is_negative = false;
+    do {
+        this->digits.push_back(digits % BigInteger::BASE);
+            digits /= BigInteger::BASE;
+    } while (digits != 0);
 }
 
+bool operator==(const BigInteger& left, const BigInteger& right) {
+        if (left.is_negative != right.is_negative)
+            return false;
 
-char* resultOperation(string& line1, string& line2, char& operation){
+        if (left.digits.empty())
+            return true ? (right.digits.empty() || (right.digits.size() == 1 && right.digits[0] == 0)) : false;
+
+        if (right.digits.empty())
+            return true ? (left.digits.size() == 1 && left.digits[0] == 0) : false;
+
+        if (left.digits.size() != right.digits.size())
+            return false;
+            
+        for (size_t i = 0; i < left.digits.size(); ++i)
+            if (left.digits[i] != right.digits[i])
+                return false;
+
+        return true;
+}
+
+void resultOperation(string& line1, string& line2, char& operation, vector<uint16_t>& res){
     string l1 = line1;
     string l2 = line1;
-    char *res;
+    BigInteger left(line1);
+    BigInteger right(line2);
+
     switch (operation) {
     case '+':
-        *res = 'vd';
+
         break;
     case '-':
-        *res = 'vd';
+
         break;
     case '*':
-        *res = 'vd';
+
         break;
     case '/':
-        *res = 'vd';
+
         break;
     case '^':
-        *res = 'vd';
+
         break;
     case '<':
-        *res = 'vd';
+
         break;
     case '>':
-        *res = 'vd';
+
         break;
     case '=':
-        *res = 'vd'.c_str();
+
         break;
     default:
         cout << "Error";
         break;
     }
-    return &res;
 }
 
 int main() {
     string line1;
     string line2;
-    char *result = nullptr;
+    std::vector<uint16_t> result;
     char operation;
 
      while (true) {
         cin >> line1;
         cin >> line2;
         cin >> operation;
-        result = resultOperation(line1, line2, operation);
-        cout << result;
+        resultOperation(line1, line2, operation, result);
+        cout << result.size();
     }
     return 0;
 }
